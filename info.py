2ba167940eb33b92f1762ed31c289ea196b322bd
hybridvamp <zainulhibath24@gmail.com>
2023-09-04 14:23:21 +0000

    no ai

--- info.py
+++ info.py
@@ -97,8 +97,8 @@ IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 
 #Openai
 AI = is_enabled((environ.get("AI","False")), False)
-OPENAI_API = environ.get("OPENAI_API"," ")
-AI_LOGS = int(environ.get("AI_LOGS"," ")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
+OPENAI_API = environ.get("OPENAI_API","0")
+AI_LOGS = int(environ.get("AI_LOGS","-1001778730930")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
 
 
 #Auto approve
