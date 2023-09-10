72eb9ebd1c81e35cbaf2827ebcf9360b8c375d8e
hybridvamp <zainulhibath24@gmail.com>
2023-09-10 08:15:34 +0000

    uddd

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -1,8 +1,10 @@
 import os
 import asyncio
+from Script import script
 from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
-from info import CHAT_ID, TEXT, APPROVED 
+from database.users_chats_db import db
+from info import CHAT_ID, TEXT, APPROVED, LOG_CHANNEL
 
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
@@ -13,3 +15,6 @@ async def autoapprove(client, message: ChatJoinRequest):
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         await client.send_message(chat_id=user.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        if not await db.is_user_exist(user.id):
+            await db.add_user(user.id, user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
