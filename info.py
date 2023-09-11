602180df3eebab57af84a414aa1b50cf1efd66a7
hybridvamp <zainulhibath24@gmail.com>
2023-09-11 06:30:43 +0000

    -1001965567679

--- info.py
+++ info.py
@@ -103,7 +103,7 @@ AI_LOGS = int(environ.get("AI_LOGS","-1001778730930")) #GIVE YOUR NEW LOG CHANNE
 
 
 #Auto approve
-CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '-1001708267003').split()]
+CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '-1001965567679').split()]
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour request has been approved")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
